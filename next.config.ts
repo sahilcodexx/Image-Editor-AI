6c316887fed197a5211168b2aaa14eaca2717e7c
Sahil Singh <sahil171759@gmail.com>
2026-02-14 00:34:13 +0530

    Disable worker threads

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   experimental: {
     workerThreads: false,
     cpus: 1,
